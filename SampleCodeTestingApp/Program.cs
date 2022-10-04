8d4f519a5f9ab24919dd0ddce5abf717e9cd357f
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2022-10-04 07:17:29 +0300

    //compare the input to the string "Hello" and ignore case

--- SampleCodeTestingApp/Program.cs
+++ SampleCodeTestingApp/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+using static System.Console;
+using static System.StringComparison;
+
+string? input  = ReadLine();
+
+
+//compare the input to the string "Hello" and ignore case
+WriteLine(input!.Contains("Hello", OrdinalIgnoreCase) ? "Hello World!" : "Goodbye World!");
+
+ReadLine();
